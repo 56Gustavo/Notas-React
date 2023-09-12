35e4a8f311f1ab2489a40d394aac3d0b9efbb2fe
56Gustavo <56gustavogomes@gmail.com>
2023-09-12 19:37:35 -0300

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      
+      <div className="footer-right">
+        Engenharia de Software
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
